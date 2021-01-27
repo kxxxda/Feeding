27227511103c52475427be3597d4832ca7489c6f
k-ddk <rpekgus@gmail.com>
2021-01-27 17:07:46 +0900

    Menu Scene_dah
    style buttons
    style scroll
    back button

---
+++ Assets/Scripts/HairSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HairSelection : MonoBehaviour
+{
+    public GameObject genderPanel;
+    public GameObject coverPanel;
+
+    public void OnClickStylekButton() 
+    {
+        genderPanel.SetActive(!genderPanel.activeSelf);
+        coverPanel.SetActive(!coverPanel.activeSelf);
+    }
+
+    public void OnClickBackButton() 
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+
+
+}
