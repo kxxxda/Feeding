6a95433b271757cd9797b503ff0370acde923f3c
k-ddk <rpekgus@gmail.com>
2021-01-29 17:04:20 +0900

    resolution _dah
    해상도 조정

--- Assets/Scripts/MenuSelection.cs
+++ Assets/Scripts/MenuSelection.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuSelection : MonoBehaviour
 {
-    public void GameStart()
+
+        public void GameStart()
     {
         SceneManager.LoadScene("Menu Scene");
     }
