674a76aa2b2431f0fc228daeb1d2ffa839473e63
sally <sally2596@gmail.com>
2021-02-01 14:18:34 +0900

    헤어셀렉션에 데이터 매니저 추가-sally

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,48 +6,48 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     int stage; //현재 진행할 스테이지 정보 
-    public GameData gameData; 
+    public DataManager dataManager;
     public Button[] stageButton;
 
 
     private void Awake()
     {
         Debug.Log("GameManager Awake");
-
-        if (gameData.stage1 == 0)
+        dataManager = GameObject.Find("DataManager").GetComponent<DataManager>();
+        if (dataManager.data.stage1 == 0)
         {
             stageButton[0].interactable = false; // 0이면 버튼 클릭을 비활성
         }
-        if (gameData.stage2 == 0)
+        if (dataManager.data.stage2 == 0)
         {
             stageButton[1].interactable = false; // 버튼 클릭을 비활성
         }
-        if (gameData.stage3 == 0)
+        if (dataManager.data.stage3 == 0)
         {
             stageButton[2].interactable = false; // 버튼 클릭을 비활성
         }
-        if (gameData.stage4 == 0)
+        if (dataManager.data.stage4 == 0)
         {
             stageButton[3].interactable = false; // 버튼 클릭을 비활성
         }
-        if (gameData.stage5 == 0)
+        if (dataManager.data.stage5 == 0)
         {
             stageButton[4].interactable = false; // 버튼 클릭을 비활성
         }
-        if (gameData.stage6 == 0)
+        if (dataManager.data.stage6 == 0)
         {
             stageButton[5].interactable = false; // 버튼 클릭을 비활성
         }
-        if (gameData.stage7 == 0)
+        if (dataManager.data.stage7 == 0)
         {
             stageButton[6].interactable = false; // 버튼 클릭을 비활성
         }
-        if (gameData.stage8 == 0)
+        if (dataManager.data.stage8 == 0)
         {
             stageButton[7].interactable = false; // 버튼 클릭을 비활성
         }
     }
-
+   
     public void SelectStage(int sta)
     {
         stage = sta; //현재 클릭한 버튼의 스테이지 정보
