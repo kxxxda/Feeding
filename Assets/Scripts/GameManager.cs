ca9a963c40a66965ba4fa657b5355c2905e71d0a
k-ddk <rpekgus@gmail.com>
2021-02-02 22:17:09 +0900

    Revert "Menu Scene & Game Scene_dah"
    This reverts commit d36345a0f2445c5a7a947bddd3991121bb2da8d9.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,53 +8,43 @@ public class GameManager : MonoBehaviour
     int stage; //현재 진행할 스테이지 정보 
     public DataManager dataManager;
     public Button[] stageButton;
-    public Text[] coverText;
 
 
     private void Awake()
     {
         Debug.Log("GameManager Awake");
         dataManager = GameObject.Find("DataManager").GetComponent<DataManager>();
-
         if (dataManager.data.stage1 == 0)
         {
             stageButton[0].interactable = false; // 0이면 버튼 클릭을 비활성
-            coverText[0].text = "???";
         }
         if (dataManager.data.stage2 == 0)
         {
             stageButton[1].interactable = false; // 버튼 클릭을 비활성
-            coverText[1].text = "???";
         }
         if (dataManager.data.stage3 == 0)
         {
             stageButton[2].interactable = false; // 버튼 클릭을 비활성
-            coverText[2].text = "???";
         }
         if (dataManager.data.stage4 == 0)
         {
             stageButton[3].interactable = false; // 버튼 클릭을 비활성
-            coverText[3].text = "???";
         }
         if (dataManager.data.stage5 == 0)
         {
             stageButton[4].interactable = false; // 버튼 클릭을 비활성
-            coverText[4].text = "???";
         }
         if (dataManager.data.stage6 == 0)
         {
             stageButton[5].interactable = false; // 버튼 클릭을 비활성
-            coverText[5].text = "???";
         }
         if (dataManager.data.stage7 == 0)
         {
             stageButton[6].interactable = false; // 버튼 클릭을 비활성
-            coverText[6].text = "???";
         }
         if (dataManager.data.stage8 == 0)
         {
             stageButton[7].interactable = false; // 버튼 클릭을 비활성
-            coverText[7].text = "???";
         }
     }
    
