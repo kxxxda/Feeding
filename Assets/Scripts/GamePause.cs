37914af201ae806d91ced07acb8ad079575fb774
k-ddk <rpekgus@gmail.com>
2021-02-04 22:12:50 +0900

    Game Scene_dah
    슬라이더 바 추가가

--- Assets/Scripts/GamePause.cs
+++ Assets/Scripts/GamePause.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GamePause : MonoBehaviour
 {
     public GameObject pausePanel;
     public GameObject coverPanel;
+    public RectTransform rectTrans;
+    public Slider slider;
+
+    private void Update()
+    {
+        Debug.Log(rectTrans.rect.height);
+        if (rectTrans.rect.height >= 300) //content의 높이가 특정 숫자보다 커지면
+        {
+            slider.gameObject.SetActive(true);  //슬라이더바가 활성화 되고
+            slider.value += Time.deltaTime;  //흐르는 시간에 맞춰 바가 채워짐
+        }
+
+    }
 
     public void OnClickPauseButton()
     {
