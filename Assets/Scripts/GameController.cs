ec8a818d43c5dd092b38c68ff185e9d0de9e48dd
sally <sally2596@gmail.com>
2021-02-04 22:23:57 +0900

    오브젝트 풀링 + 화면 멀티터치-sally

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,29 +4,58 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hairSpray;
     private Camera mainCamera;
-    public DataManager dataManager;
+    private DataManager dataManager;
+    private ObjectManager objectManager;
+    private Touch tempTouchs;
+    private Vector2 touchPos;
+    private bool touchOn;
 
-    private void Start()
+    private void Awake()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         dataManager = GameObject.Find("Data Manager").GetComponent<DataManager>();
+        objectManager = GameObject.Find("Object Manager").GetComponent<ObjectManager>();
     }
+
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        //터치 시에
+        //touchOn = false;
+        //if (Input.touchCount > 0)
+        //{
+        //    for(int i = 0; i < Input.touchCount; i++)
+        //    {
+
+        //        tempTouchs = Input.GetTouch(0);
+        //        if (tempTouchs.phase == TouchPhase.Began)
+        //        {
+        //            touchPos = mainCamera.ScreenToWorldPoint(tempTouchs.position);
+        //            touchOn = true;
+        //            dataManager.data.clickCount[dataManager.currentStage] += 1;
+        //            SprayControl(touchPos);
+        //        }
+        //    }
+        //}
+
+
+        if (Input.GetMouseButtonDown(0))
         {
-            dataManager.data.clickCount[dataManager.currentStage]+=1;
+            dataManager.data.clickCount[dataManager.currentStage] += 1;
 
             Vector2 mousePosition = Input.mousePosition;
             mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
-
-            hairSpray.transform.position = mousePosition;
-            hairSpray.SetActive(true);
-            HairSpray h = hairSpray.GetComponent<HairSpray>();
-            h.StartSpray();
+            SprayControl(mousePosition);
         }
     }
 
+    void SprayControl(Vector2 pos)
+    {
+        GameObject hairSpray = objectManager.MakeObj("HairSpray");
+        hairSpray.transform.position = pos;
+        hairSpray.SetActive(true);
+        HairSpray h = hairSpray.GetComponent<HairSpray>();
+        h.StartSpray();
+    }
+    
 }
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    //프리팹들
+    public GameObject hairSprayPrefab;
+
+
+    //오브젝트 배열
+    GameObject[] hairSpray;
+
+
+    //타겟 오브젝트 배열
+    GameObject[] targetPool;
+
+    private void Awake()
+    {
+        hairSpray = new GameObject[20];
+
+        Generate();
+    }
+
+    void Generate()
+    {
+        GenerateLoop(hairSpray, hairSprayPrefab);
+    }
+
+    void GenerateLoop(GameObject[] my, GameObject myPrefab)
+    {
+        for (int i = 0; i < my.Length; i++)
+        {
+            my[i] = Instantiate(myPrefab);
+            my[i].SetActive(false);
+        }
+    }
+
+    public GameObject[] TypeOfObj(string type)
+    {
+        switch (type)
+        {
+            case "HairSpray":
+                return hairSpray;
+        }
+        return null;
+    }
+
+    public GameObject MakeObj(string type)
+    {
+        targetPool = TypeOfObj(type);
+
+        for (int i = 0; i < targetPool.Length; i++)
+        {
+            if (!targetPool[i].activeSelf)//비활성화 되어있다면
+            {
+                //targetPool[i].SetActive(true);
+                return targetPool[i];
+            }
+        }
+        return null;
+    }
+
+    public GameObject[] GetPool(string type)
+    {
+        targetPool = TypeOfObj(type);
+
+        return targetPool;
+    }
+}
