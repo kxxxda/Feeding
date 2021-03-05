b1b8ce1bd8abad1f70d7c0eae1aacf542153ef91
k-ddk <rpekgus@gmail.com>
2021-03-05 23:25:50 +0900

    Game Scene_dah
    Google Ads 하는 중

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class Ads : MonoBehaviour
+{
+    private BannerView bannerView;
+
+    public void Start()
+    {
+#if UNITY_ANDROID
+        string appId = "ca-app-pub-3940256099942544~6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544~2934735716";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize((initStatus) =>
+        {
+            RequestBanner();
+        });
+    }
+
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
+    }
+}
